4719350349c4f518f1a8e5bcc3128445a4b483ce
Hoong Jhang Chin <hj_chin@hotmail.com>
2019-07-13 16:06:02 +0800

    added questions & test case

---
+++ src/com/company/Chapter3/Q3_2.java
@@ -0,0 +1,43 @@
+package com.company.Chapter3;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Stack;
+
+public class Q3_2 {
+
+    public static class Stack{
+
+        private ArrayList<Integer> list = new ArrayList<>();
+        private java.util.Stack<Integer> min = new java.util.Stack<Integer>();
+
+        public Integer pop(){
+            if(list.size() == 0){
+                throw new IndexOutOfBoundsException();
+            }
+
+            Integer value = list.get(list.size()-1);
+            if(value <= min.peek()){
+                min.pop();
+            }
+
+            list.remove(list.size()-1);
+            return value;
+        }
+
+        public void push(Integer i){
+            if(min.empty() || i < min.peek()){
+                min.push(i);
+            }
+            list.add(i);
+        }
+
+        public Integer min(){
+            return min.peek();
+        }
+
+        public int size(){
+            return list.size();
+        }
+    }
+}

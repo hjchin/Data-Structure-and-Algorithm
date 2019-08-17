b658e2189bd5b531cd9732036d2ce433c168ca47
Hoong Jhang Chin <hj_chin@hotmail.com>
2019-08-17 17:32:19 +0800

    added questions & test case

--- src/com/company/chapter4/Q4_6.java
+++ src/com/company/chapter4/Q4_6.java
@@ -1,75 +1,48 @@
 package com.company.chapter4;
 
-import java.util.ArrayList;
-
 import static java.util.Objects.isNull;
 
 public class Q4_6 {
 
-    public static TreeNode getNextNode(TreeNode root, TreeNode target){
-        ArrayList<TreeNode> parents = new ArrayList<>();
-        parents.add(null);
-        return traverse(parents,root,target);
+    public static void inOrderTraverse(TreeNode node){
+        if(isNull(node)) return;
+        inOrderTraverse(node.leftNode);
+        System.out.println("node "+node.value);
+        inOrderTraverse(node.rightNode);
     }
 
-    public static TreeNode traverse(ArrayList<TreeNode> parents, TreeNode node, TreeNode target){
+    private Boolean found = false;
+    private TreeNode nextNode = null;
+    private TreeNode target = null;
 
-        if(isNull(node)) return null;
+    public void searchNextNode(TreeNode node, TreeNode target){
+       found = false;
+       this.target = target;
+       nextNode = null;
+       searchInOrder(node);
+    }
 
-        TreeNode parent = getParentOf(parents, node);
+    private void searchInOrder(TreeNode node){
+        if(isNull(node) || !isNull(nextNode)) return;
 
-        if(!isNull(node.leftNode) && !node.leftNode.visited){
-            if(node.leftNode.compare(target)){
-                return node;
-            }
+        searchInOrder(node.leftNode);
 
-            node.leftNode.visited = true;
-
-            if(!isNull(node.leftNode.leftNode) && !node.leftNode.leftNode.visited){
-                parents.add(node);
-                return traverse(parents, node.leftNode, target);
-            }else if(!isNull(node.leftNode.rightNode)){
-                parents.add(node);
-                return traverse(parents, node.leftNode, target);
-            }else{
-                return traverse(parents, node, target);
-            }
-        }
+        System.out.println("node "+node.value);
 
         if(node.compare(target)){
-            return parent;
+            found = true;
+        }else if(found){
+            System.out.println("next node found "+node.value);
+            if(isNull(nextNode)) nextNode = node;
         }
 
-        if(!isNull(node.rightNode) && !node.rightNode.visited){
-            node.visited = true;
-            if(node.rightNode.compare(target)){
-                return getParentOf(parents, node);
-            }else{
-                TreeNode pp = getParentOf(parents, parent);
-                return traverse(parents, pp, target);
-            }
-        }
-
-        return null;
+        searchInOrder(node.rightNode);
     }
 
-    private static TreeNode getParentOf(ArrayList<TreeNode> parents, TreeNode node){
-
-        for(int i=0;i<parents.size();i++){
-            if(!isNull(parents.get(i)) && parents.get(i).compare(node)){
-                int j = i - 1;
-                if(j<parents.size()){
-                    return parents.get(j);
-                }
-                return null;
-            }
-        }
-
-        if(parents.size()-1 >= 0){
-            return parents.get(parents.size()-1);
-        }else
-            return null;
+    public TreeNode getNextNode(){
+        return nextNode;
     }
+
 
 
 }
--- src/com/company/chapter4/Q4_6Test.java
+++ src/com/company/chapter4/Q4_6Test.java
@@ -9,48 +9,86 @@ import static junit.framework.TestCase.assertTrue;
 public class Q4_6Test {
 
     @Test
-    public void test(){
+    public void testInOrderTraverse(){
         TreeNode root = new TreeNode(10);
-        TreeNode nl = new TreeNode(8);
-        assertEquals(Q4_6.getNextNode(root, nl), null);
+        Q4_6.inOrderTraverse(root);
+        assertTrue(true);
     }
 
     @Test
-    public void test2(){
+    public void testInOrderTraverse1(){
         TreeNode root = new TreeNode(10);
-        assertEquals(Q4_6.getNextNode(root, root), null);
+        root.leftNode = new TreeNode(8);
+        root.rightNode = new TreeNode(20);
+        Q4_6.inOrderTraverse(root);
+        assertTrue(true);
     }
 
     @Test
-    public void test3(){
+    public void testInOrderTraverse2(){
+        TreeNode root = new TreeNode(10);
+        root.leftNode = new TreeNode(8);
+        root.leftNode.leftNode = new TreeNode(7);
+        root.leftNode.rightNode = new TreeNode(9);
+        root.rightNode = new TreeNode(20);
+        Q4_6.inOrderTraverse(root);
+        assertTrue(true);
+    }
+
+
+    @Test
+    public void testNextNode(){
+        TreeNode root = new TreeNode(10);
+        Q4_6 Q4_6 = new Q4_6();
+        Q4_6.searchNextNode(root, root);
+        assertEquals(null, Q4_6.getNextNode());
+    }
+
+
+
+    @Test
+    public void testNextNode1(){
+
+        /*
+         *       10
+         *      /
+         *     8
+         * */
+
         TreeNode root = new TreeNode(10);
         TreeNode nl = new TreeNode(8);
         root.leftNode = nl;
-        assertTrue(Q4_6.getNextNode(root, nl).compare(root));
+
+        Q4_6 Q4_6 = new Q4_6();
+        Q4_6.searchNextNode(root, nl);
+        assertEquals(root,Q4_6.getNextNode());
+
+        Q4_6.searchNextNode(root, root);
+        assertEquals(null,Q4_6.getNextNode());
     }
 
     @Test
-    public void test4(){
+    public void testNextNode2(){
+
+        /*
+         *       10
+         *      /  \
+         *     8   20
+         * */
+
         TreeNode root = new TreeNode(10);
         TreeNode nl = new TreeNode(8);
-        TreeNode nr = new TreeNode(18);
+        TreeNode nr = new TreeNode(20);
         root.leftNode = nl;
         root.rightNode = nr;
-        assertTrue(Q4_6.getNextNode(root, nl).compare(root));
+
+        Q4_6 Q4_6 = new Q4_6();
+        Q4_6.searchNextNode(root, root);
+        assertEquals(nr,Q4_6.getNextNode());
     }
 
     @Test
-    public void test5(){
-        TreeNode root = new TreeNode(10);
-        TreeNode nl = new TreeNode(8);
-        TreeNode nr = new TreeNode(18);
-        root.leftNode = nl;
-        root.rightNode = nr;
-        assertEquals(null, Q4_6.getNextNode(root, nr));
-    }
-
-    @Test
-    public void test6(){
+    public void testNextNode3(){
 
         /*
                 20
@@ -67,43 +105,150 @@ public class Q4_6Test {
         root.leftNode = nl;
         root.rightNode = nr;
         root.leftNode.leftNode = nll;
-        assertEquals(nl,Q4_6.getNextNode(root, nll));
+
+        Q4_6 Q4_6 = new Q4_6();
+        Q4_6.searchNextNode(root, nll);
+        assertEquals(nl,Q4_6.getNextNode());
+
+        Q4_6.searchNextNode(root, nl);
+        assertEquals(root,Q4_6.getNextNode());
+
+        Q4_6.searchNextNode(root, nr);
+        assertEquals(null,Q4_6.getNextNode());
     }
 
     @Test
-    public void test7(){
+    public void testNextNode4(){
+
         /*
                 20
                /  \
               10  30
              /  \
-            4    25
+            4    12
          */
 
         TreeNode root = new TreeNode(20);
-        TreeNode nlr = new TreeNode(25);
-        root.leftNode = new TreeNode(10);
-        root.rightNode = new TreeNode(30);
-        root.leftNode.leftNode = new TreeNode(4);
+        TreeNode nl = new TreeNode(10);
+        TreeNode nr = new TreeNode(30);
+        TreeNode nll = new TreeNode(4);
+        TreeNode nlr = new TreeNode(12);
+        root.leftNode = nl;
+        root.rightNode = nr;
+        root.leftNode.leftNode = nll;
         root.leftNode.rightNode = nlr;
-        assertEquals(root, Q4_6.getNextNode(root, nlr));
+        Q4_6 Q4_6 = new Q4_6();
+        Q4_6.searchNextNode(root, root.leftNode.rightNode);
+        assertEquals(root, Q4_6.getNextNode());
     }
 
     @Test
-    public void test8(){
+    public void testNextNode5(){
+
         /*
                 20
                /  \
               10  30
-                \
-                25
+               \
+                12
          */
 
         TreeNode root = new TreeNode(20);
-        TreeNode nlr = new TreeNode(25);
-        root.leftNode = new TreeNode(10);
-        root.rightNode = new TreeNode(30);
+        TreeNode nl = new TreeNode(10);
+        TreeNode nr = new TreeNode(30);
+        TreeNode nlr = new TreeNode(12);
+        root.leftNode = nl;
+        root.rightNode = nr;
         root.leftNode.rightNode = nlr;
-        assertEquals(root, Q4_6.getNextNode(root, nlr));
+        Q4_6 Q4_6 = new Q4_6();
+        Q4_6.searchNextNode(root, root.leftNode.rightNode);
+        assertEquals(root, Q4_6.getNextNode());
+
+        Q4_6.searchNextNode(root, nr);
+        assertEquals(null, Q4_6.getNextNode());
+    }
+
+    @Test
+    public void testNextNode6(){
+
+        /*
+                20
+               /   \
+              10   30
+                  /
+                 29
+         */
+
+        TreeNode root = new TreeNode(20);
+        TreeNode nl = new TreeNode(10);
+        TreeNode nr = new TreeNode(30);
+        TreeNode nrl = new TreeNode(29);
+        root.leftNode = nl;
+        root.rightNode = nr;
+        root.rightNode.leftNode = nrl;
+        Q4_6 Q4_6 = new Q4_6();
+        Q4_6.searchNextNode(root, nrl);
+        assertEquals(nr, Q4_6.getNextNode());
+
+        Q4_6.searchNextNode(root, nr);
+        assertEquals(null, Q4_6.getNextNode());
+    }
+
+    @Test
+    public void testNextNode7(){
+
+        /*
+                20
+               /   \
+              10   30
+                  /  \
+                 29   35
+         */
+
+        TreeNode root = new TreeNode(20);
+        TreeNode nl = new TreeNode(10);
+        TreeNode nr = new TreeNode(30);
+        TreeNode nrl = new TreeNode(29);
+        TreeNode nrr = new TreeNode(35);
+        root.leftNode = nl;
+        root.rightNode = nr;
+        root.rightNode.leftNode = nrl;
+        root.rightNode.rightNode = nrr;
+
+        Q4_6 Q4_6 = new Q4_6();
+
+        Q4_6.searchNextNode(root, nr);
+        assertEquals(nrr, Q4_6.getNextNode());
+
+        Q4_6.searchNextNode(root, nrr);
+        assertEquals(null, Q4_6.getNextNode());
+    }
+
+    @Test
+    public void testNextNode8(){
+
+        /*
+                20
+               /   \
+              10   30
+                     \
+                     35
+         */
+
+        TreeNode root = new TreeNode(20);
+        TreeNode nl = new TreeNode(10);
+        TreeNode nr = new TreeNode(30);
+        TreeNode nrr = new TreeNode(35);
+        root.leftNode = nl;
+        root.rightNode = nr;
+        root.rightNode.rightNode = nrr;
+
+        Q4_6 Q4_6 = new Q4_6();
+
+        Q4_6.searchNextNode(root, nr);
+        assertEquals(nrr, Q4_6.getNextNode());
+
+        Q4_6.searchNextNode(root, nrr);
+        assertEquals(null, Q4_6.getNextNode());
     }
 }

058376fe94401cf9e06fcb2d7a5b26ed495a2828
Hoong Jhang Chin <hj_chin@hotmail.com>
2019-12-19 14:55:40 +0800

    Bit Operation

--- src/com/company/chapter5/BitOperation.java
+++ src/com/company/chapter5/BitOperation.java
@@ -31,4 +31,89 @@ public class BitOperation {
         }
     }
 
+    static int maskRight(int position){
+        return (1<<position) -1;
+    }
+
+    static int maskLeft(int position){
+        int val = ~0;
+        return val << (position +1);
+    }
+
+    static int maskZeroInBetween(int i, int j){
+        return maskLeft(j) | maskRight(i);
+    }
+
+    static String convertIntegerToBinary(int value){
+
+        String binary = "";
+        int remainder;
+        int quotient = value;
+
+        do{
+            int temp = quotient;
+            quotient = temp / 2;
+            remainder = temp % 2;
+            binary = remainder + binary;
+        }while(quotient != 0);
+
+        return binary;
+    }
+
+    static String convertDecimalToBinary(double value, int decimalLimit){
+
+        StringBuilder binaryBuilder = new StringBuilder();
+        binaryBuilder.append("0.");
+        double next = value;
+
+        while (binaryBuilder.toString().length()-2 < decimalLimit){
+            next = next * 2;
+            int integer = (int)next;
+
+            if(integer == 1){
+                binaryBuilder.append("1");
+                next = next  - 1;
+            }else{
+                binaryBuilder.append("0");
+            }
+
+            if(next == 0 || next == 1){
+                break;
+            }
+        }
+
+        return binaryBuilder.toString();
+    }
+
+    static int convertBinaryToDecimal(String value){
+        double sum = 0;
+
+        for(int i=value.length()-1, j=0;i>-1;i--,j++){
+            char c = value.charAt(i);
+            if(c == '1'){
+                sum += Math.pow(2,j);
+            }
+        }
+
+        return (int)sum;
+    }
+
+
+
+    static double convertFractionalBinaryToDecimal(String value){
+
+        String[] split = value.split("\\.");
+
+        if(split.length != 2){
+            return -1d;
+        }
+
+        String fractional = split[1];
+        double acc = 0;
+        for(int i = fractional.length()-1;i>-1;i--){
+            acc = (Integer.parseInt(String.valueOf(fractional.charAt(i))) + acc)/2;
+        }
+
+        return acc;
+    }
 }
--- src/com/company/chapter5/BitOperationTest.java
+++ src/com/company/chapter5/BitOperationTest.java
@@ -66,4 +66,76 @@ public class BitOperationTest {
         assertEquals("10", Integer.toString(value, 2));
     }
 
+    @Test
+    public void testMaskRight(){
+        int val = BitOperation.maskRight(2);  //100 as input
+        System.out.println(Integer.toString(val, 2));//11
+        assertEquals("11",Integer.toString(val, 2));
+    }
+
+    @Test
+    public void testMaskLeft(){
+        int val = BitOperation.maskLeft(2);//11111000
+        assertEquals("11111000",Integer.toBinaryString(val & 0xFF));
+    }
+
+    @Test
+    public void testMaskZeroInBetween(){
+        int val = BitOperation.maskZeroInBetween(2, 6);
+        assertEquals("10000011",Integer.toBinaryString(val & 0xFF));
+    }
+
+    @Test
+    public void testConvertDecimalToBinary(){
+        String value = BitOperation.convertIntegerToBinary(0);
+        assertEquals("0",value);
+
+        value = BitOperation.convertIntegerToBinary(1);
+        assertEquals("1",value);
+
+        value = BitOperation.convertIntegerToBinary(10);
+        assertEquals("1010",value);
+
+        value = BitOperation.convertIntegerToBinary(100);
+        assertEquals("1100100",value);
+    }
+
+    @Test
+    public void testConvertFractionalDecimalToBinary(){
+        String value = BitOperation.convertDecimalToBinary(0.375, 10);
+        assertEquals("0.011", value);
+
+        value = BitOperation.convertDecimalToBinary(0.37, 10);
+        assertEquals("0.0101111010", value);
+    }
+
+    @Test
+    public void testConvertBinaryToDecimal(){
+        int value = BitOperation.convertBinaryToDecimal("0");
+        assertEquals(0, value);
+
+        value = BitOperation.convertBinaryToDecimal("1");
+        assertEquals(1, value);
+
+        value = BitOperation.convertBinaryToDecimal("1010");
+        assertEquals(10,value);
+
+        value = BitOperation.convertBinaryToDecimal("1100100");
+        assertEquals(100,value);
+    }
+
+    @Test
+    public void testConvertFractionBinaryToDecimal(){
+        double value = BitOperation.convertFractionalBinaryToDecimal("0.0");
+        assertEquals(0.0d, value);
+
+        value = BitOperation.convertFractionalBinaryToDecimal("0.0101");
+        assertEquals(0.3125d, value);
+
+        value = BitOperation.convertFractionalBinaryToDecimal("0.111000");
+        assertEquals(0.875d, value);
+    }
+
+
+
 }

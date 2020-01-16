4e9d391d4b66acd4f1ede853c1ed3d5e148d00d6
Hoong Jhang Chin <hj_chin@hotmail.com>
2020-01-16 13:03:30 +0800

    Chapter 6

---
+++ src/com/company/chapter6/Q6_3.java
@@ -0,0 +1,29 @@
+package com.company.chapter6;
+
+public class Q6_3 {
+
+    /*
+      _ _ _ _ _ _ _ _
+   1 |_|_|_|_|_|_|_|X|
+   2 |_|_|_|_|_|_|_|_|
+   3 |_|_|_|_|_|_|_|_|
+   4 |_|_|_|_|_|_|_|_|
+   5 |_|_|_|_|_|_|_|_|
+   6 |_|_|_|_|_|_|_|_|
+   7 |_|_|_|_|_|_|_|_|
+   8 |X|_|_|_|_|_|_|_|
+      1 2 3 4 5 6 7 8
+
+   There is an 8x8 chessboard in which two diagonally opposite corners have been cut off.
+   You are given 31 dominos, and a single domino can cover exactly two squares. Can you use the 31
+   dominos to cover the entire board? Prove your answer (by providing an example or showing why
+   it's impossible).
+
+   total grid = 8X8 - 2 = 62
+   31 x 2 = 62
+
+   on wich row or column at 8, it is always one grid empty.
+
+
+     */
+}

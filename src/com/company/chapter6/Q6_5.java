7916fbe9d9ca7844eb6a0e92acfd152b833cfe4a
Hoong Jhang Chin <hj_chin@hotmail.com>
2020-01-16 17:17:39 +0800

    Chapter 6

---
+++ src/com/company/chapter6/Q6_5.java
@@ -0,0 +1,19 @@
+package com.company.chapter6;
+
+public class Q6_5 {
+
+    /*
+
+        You have a five-quart jug, a three-quart jug, and an unlimited supply of water (but
+        no measuring cups). How would you come up with exactly four quarts of water? Note that the jugs
+        are oddly shaped, such that filling up exactly "half" of the jug would be impossible.
+
+        5 quart jug, 3 quart jug
+
+        1. Full 5 quart jug, pour into 3 quart jug, get 2 quart water.
+        2. pour 2 quart water into 3 quart jug. 3 quart jug is with 2 quart water. balance in 3 quart jug is 1 quart.
+        3. Full 5 quart jug, pour into 3 quart jug. Only 1 quart of water being poured into 3 quart jug.
+        4. 5 quart jug now is with 4 quart of water.
+
+     */
+}

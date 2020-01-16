cf70517bdd9923adfb34d551d75c64d82d9aba4e
Hoong Jhang Chin <hj_chin@hotmail.com>
2020-01-16 22:54:55 +0800

    Chapter 6

---
+++ src/com/company/chapter6/Q6_7.java
@@ -0,0 +1,30 @@
+package com.company.chapter6;
+
+public class Q6_7 {
+
+    /*
+
+    In the new post-apocalyptic world, the world queen is desperately concerned
+    about the birth rate. Therefore, she decrees that all families should ensure that they have one girl or
+    else they face massive fines. If all families abide by this policy that is, they have continue to have
+    children until they have one girl, at which point they immediately stop-what will the gender ratio
+    of the new generation be? (Assume that the odds of someone having a boy or a girl on any given
+    pregnancy is equal.) Solve this out logically and then write a computer simulation of it.
+
+
+    p(g) = 1/2
+
+    if first child = boy
+    p(g^2) = 1/2 * 1/2
+
+    p(g^n) = (1/2)^n
+
+    p(b for n) = 1 - (1/2)^n
+
+    ratio = (1 - (1/2)^n) / (1/2)^n
+
+
+     */
+
+
+}

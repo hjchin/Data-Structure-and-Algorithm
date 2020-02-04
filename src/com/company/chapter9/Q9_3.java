5f419e90462a4fcabf963d382920980e680b9fe9
Hoong Jhang Chin <hj_chin@hotmail.com>
2020-02-04 15:54:41 +0800

    Chapter 9

---
+++ src/com/company/chapter9/Q9_3.java
@@ -0,0 +1,13 @@
+package com.company.chapter9;
+
+public class Q9_3 {
+
+    /*
+
+        Web Crawler: If you were designing a web crawler, how would you avoid getting into infinite loops?
+
+        Answer:
+        Use graph to represent the documents on the Internet. Each visited node is marked with visited flag.
+
+     */
+}

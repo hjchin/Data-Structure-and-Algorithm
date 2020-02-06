5af0ccafd5351ca84120719643e6d3952b576a10
Hoong Jhang Chin <hj_chin@hotmail.com>
2020-02-06 22:19:53 +0800

    Chapter 9

---
+++ src/com/company/chapter9/Q9_8.java
@@ -0,0 +1,23 @@
+package com.company.chapter9;
+
+public class Q9_8 {
+
+    /*
+        Pastebin: Design a system like Pastebin, where a user can enter a piece of text and get a randomly
+        generated URL for public access.
+
+        Answer:
+        1. visualize the GUI.
+           a. there is an editor, submit button.
+           b. once user click on submit button, system generates a random URL.
+           c. there is no edit button.
+        2. Behaviour
+           a. the content will expire.
+        3. Design
+           a. use a table to store the link and the content and expire datetime.
+           b. remove row daily whenever data expires.
+           c. redirect to 404 page if content no found. url is www.domain.com/text/xxxxx
+
+
+     */
+}

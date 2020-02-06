18468f3d956006f14096c0d264e004367a874e0c
Hoong Jhang Chin <hj_chin@hotmail.com>
2020-02-06 21:53:04 +0800

    Chapter 9

---
+++ src/com/company/chapter9/Q9_7.java
@@ -0,0 +1,29 @@
+package com.company.chapter9;
+
+public class Q9_7 {
+
+    /*
+
+    Personal Financial Manager: Explain how you would design a personal financial manager (like
+    Mint.com). This system would connect to your bank accounts, analyze your spending habits, and
+    make recommendations.
+
+    Answer:
+    1. analyze debit and credit.
+    2. analyze debit source, income source. Gave income source category.
+    3. analyze credit source, expenses source. Gave expense source category.
+    4. analyze credit card spending as credit source.
+    4. Calculate the % of each category of expenses
+    5. build recommendation for expenses category. for instance,
+       a. recommend to get discounted item for grocery shopping.
+       b. recommend reduce dining expenses if dining spending exceeds certain amount.
+       c. also recommend saving percentage.
+       d. send notification when expenses exceeds certain amount.
+
+
+    Design:
+    1. visualize the expenses as 100% in form of pie.
+    2. Clicking each portion bring user to listing of transaction under that category.
+
+     */
+}

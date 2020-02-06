5463aba01cbc68fc2750c5ab61a6cc07ad519760
Hoong Jhang Chin <hj_chin@hotmail.com>
2020-02-06 09:16:37 +0800

    Chapter 9

---
+++ src/com/company/chapter9/Q9_6.java
@@ -0,0 +1,19 @@
+package com.company.chapter9;
+
+public class Q9_6 {
+
+    /*
+
+    Sales Rank: A large eCommerce company wishes to list the best-selling products, overall and by
+    category. For example, one product might be the #1056th best-selling product overall but the #13th
+    best-selling product under "Sports Equipment" and the #24th best-selling product under "Safety:·
+    Describe how you would design this system.
+
+    Answer:
+    1. Product ID to be stored in multiple ranking list.
+    2. We have product category as ranking list. They are Sport Equipment, Safety.
+    3. transaction data will be processed at end of each day to update ranking table.
+    4. Overall ranking will considers each item and rank them without category.
+
+     */
+}

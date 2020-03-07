f7636755725933dc94d67899ab7e7c83bedc638b
Hoong Jhang Chin <hj_chin@hotmail.com>
2020-03-07 11:54:27 +0800

    Chapter 10

---
+++ src/com/company/chapter10/Q10_6.java
@@ -0,0 +1,13 @@
+package com.company.chapter10;
+
+public class Q10_6 {
+
+    /*
+    Sort Big File: Imagine you have a 20 GB file with one string per line. Explain how you would sort
+    the file
+
+     answer: external merge sort. https://www.youtube.com/watch?v=ATK74YSzwxg
+     */
+
+
+}

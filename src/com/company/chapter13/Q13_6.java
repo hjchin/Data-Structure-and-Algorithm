35aa0e6d79e09bb512d20bcfba060a693027e694
Hoong Jhang Chin <hj_chin@hotmail.com>
2020-03-08 14:44:47 +0800

    Chapter 13

---
+++ src/com/company/chapter13/Q13_6.java
@@ -0,0 +1,16 @@
+package com.company.chapter13;
+
+/*
+
+    13.6 Object Reflection: Explain what object reflection is in Java and why it is useful.
+
+    Answer:
+    Object reflection
+    1. allow u to access class information on runtime
+    2. create instance from class name
+    3. getting and setting object fields directly regardless of the access modifier.
+
+
+ */
+public class Q13_6 {
+}

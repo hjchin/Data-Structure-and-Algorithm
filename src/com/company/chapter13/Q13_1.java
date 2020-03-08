6f3b91438edbf2f1db565eb4939e07ece81eb19c
Hoong Jhang Chin <hj_chin@hotmail.com>
2020-03-08 12:18:51 +0800

    Chapter 13

---
+++ src/com/company/chapter13/Q13_1.java
@@ -0,0 +1,14 @@
+package com.company.chapter13;
+
+/*
+    Private Constructor: In terms of inheritance, what is the effect of keeping a constructor private?
+
+    Answer:
+    it cannot be instantiated outside the class. Consumer only can create this class via method provided in this class.
+    It could be only accessible from
+ */
+
+public class Q13_1 {
+
+
+}

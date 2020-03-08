a171e473d646a12566a26ee1622e869d289b3fc6
Hoong Jhang Chin <hj_chin@hotmail.com>
2020-03-08 13:44:07 +0800

    Chapter 13

---
+++ src/com/company/chapter13/Q13_4.java
@@ -0,0 +1,13 @@
+package com.company.chapter13;
+
+/*
+    13.4 Generics vs. Templates: Explain the difference between templates in C ++ and generics in Java.
+
+    Answer:
+    This is quite different from C++. In C++, templates are essentially a glorified macro set, with the compiler
+    creating a new copy of the template code for each type.
+ */
+public class Q13_4 {
+
+
+}

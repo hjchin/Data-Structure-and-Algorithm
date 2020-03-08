ddf5556884c5b827fe261c13f8645878db1b72da
Hoong Jhang Chin <hj_chin@hotmail.com>
2020-03-08 14:26:29 +0800

    Chapter 13

---
+++ src/com/company/chapter13/Q13_5.java
@@ -0,0 +1,20 @@
+package com.company.chapter13;
+
+/*
+    TreeMap, HashMap, LinkedHashMap: Explain the differences between TreeMap, HashMap, and
+    LinkedHashMap. Provide an example of when each one would be best.
+
+    Answer:
+    1. TreeMap. sorted item. time complexity is O(lg N) for insertion and retrieval.
+    2. HashMap. order is arbitrary. insertion and retrieval time complexity is O(1).
+    2. LinkedHashMap. order follow the insertion order. Insertion and retrieval time complexity is O(1).
+
+    Usage:
+    1. if you need order, use treeMap.
+    2. if you need insertion order, use linkedHashMap.
+    3. if hashMap as default. no order is required.
+
+ */
+public class Q13_5 {
+
+}

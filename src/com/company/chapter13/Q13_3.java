1cae7a7cd3319c7b3441185c218e0fc1d8eb31b9
Hoong Jhang Chin <hj_chin@hotmail.com>
2020-03-08 12:38:18 +0800

    Chapter 13

---
+++ src/com/company/chapter13/Q13_3.java
@@ -0,0 +1,16 @@
+package com.company.chapter13;
+
+/*
+    13.3 Final, etc.: What is the difference between final, finally, and finalize?
+
+    Answer:
+    final on variable - single copy of such variable. It cannot be reassigned value again.
+    final on method - it cannot be overriden.
+    final on class - it cannot be extend.
+
+    finally - use to logic branch to execute regardless there is any exception occurs in try/catch block
+
+    finalize - garbage collector call finalize to release memory and destroy resource.
+ */
+public class Q13_3 {
+}

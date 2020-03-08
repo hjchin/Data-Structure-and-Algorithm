fa8e78501613532eec69ffcb1329d6fbc2707869
Hoong Jhang Chin <hj_chin@hotmail.com>
2020-03-08 12:24:47 +0800

    Chapter 13

---
+++ src/com/company/chapter13/Q13_2.java
@@ -0,0 +1,13 @@
+package com.company.chapter13;
+
+/*
+    Return from Finally: In Java, does the finally block get executed if we insert a return statement
+    inside the try block of a try-catch-finally?
+
+    Answer: Yes. In some cases, it won't executed such as
+    1. VM terminates in try catch block.
+    2. Thread terminates in try catch block
+
+ */
+public class Q13_2 {
+}
